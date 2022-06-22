9b9f12962fdaefbe2d9f243aefc6f0630d252f4d
Archie Martin <98323890+archiemartini@users.noreply.github.com>
2022-06-22 18:06:58 +0100

    + extra pipes that denote depo/withd in Statement

--- spec/statement.test.js
+++ spec/statement.test.js
@@ -11,24 +11,17 @@ describe('Statement class', () => {
     expect(statement.transactions).toEqual([]);
   })
 
-  it('can add a new object to transactions array', () => {
-    statement.addTransaction('11/05/23', 500, 1500);
-    expect(statement.transactions[0].date).toEqual('11/05/23');
-    expect(statement.transactions[0].debit).toEqual(500);
-    expect(statement.transactions[0].balance).toEqual(1500);
 
-  })
-
-  it('can return its transactions', () => {
-    statement.addTransaction('11/05/23', 500, 1500);
-    expect(statement.transactions).toEqual([{balance: 1500, date: "11/05/23", debit: 500}]);
+  it('can add a new transaction string to the array', () => {
+    statement.addDepositTransaction('11/05/23', 500, 1500);
+    expect(statement.transactions).toEqual(["11/05/23 || 500 || || 1500"]);
   })
 
   it('stores transactions in reverse chronological order', () => {
-    statement.addTransaction('11/05/23', 750, 750)
-    statement.addTransaction('13/05/23', 250, 500)
-    expect(statement.transactions[0]).toEqual({balance: 500, date: "13/05/23", debit: 250})
-    expect(statement.transactions[1]).toEqual({balance: 750, date: "11/05/23", debit: 750})
+    statement.addDepositTransaction('11/05/23', 750, 750)
+    statement.addWithdrawalTransaction('13/05/23', 250, 500)
+    expect(statement.transactions[0]).toEqual("13/05/23 || || 250 || 500")
+    expect(statement.transactions[1]).toEqual("11/05/23 || 750 || || 750")
   })
   
 })
